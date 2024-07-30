8958d1fcbef0838e7d27d8151b74d6d4f1ef3174
cujo <akash2110742@ssn.edu.in>
2024-07-30 13:23:34 +0530

    07/30/24

--- server/nixos/modules/bash.nix
+++ server/nixos/modules/bash.nix
@@ -9,8 +9,11 @@
                 in {
                         
                         rebuild = "sudo nixos-rebuild switch --flake '${flakePath}#nixos-server'";
-			docker_update = "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once ";         
+			docker_update = "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once ";
+			radarr_update = "docker exec -it recyclarr recyclarr sync -c /config/radarr.yml";
+			anime_update = "docker exec -it recyclarr recyclarr sync -c /config/sonarr_anime.yml";
                 };
+			
         };
 
 }
