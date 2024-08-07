71b12cc53c1f97b51221ba6d244c1dd3be8d7024
cujo <akash2110742@ssn.edu.in>
2024-08-07 17:08:01 +0530

    08/07/24

--- lenovo-laptop/nixos/modules/vm.nix
+++ lenovo-laptop/nixos/modules/vm.nix
@@ -3,7 +3,7 @@
 { 
   
   boot.kernelParams = [ "amd_iommu=on" "iommu=pt" ]; 
-  boot.kernelModules = [ "kvm-amd" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+  boot.kernelModules = [ "kvm-amd" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" "nbd" ];
   boot.extraModprobeConfig = ''
   options vfio-pci ids=10de:2560,10de:228e
   options kvm ignore_msrs=1
@@ -14,6 +14,7 @@
       echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
     done
     modprobe -i vfio-pci
+    modprobe nbd max_part=16
     # Setup Looking Glass shared memory object
     touch /dev/shm/looking-glass
     chown cujo:kvm /dev/shm/looking-glass
