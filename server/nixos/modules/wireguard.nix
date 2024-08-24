73ce25099fb58f5f2925049da7b1eab6cc14fb3e
cujo <akash2110742@ssn.edu.in>
2024-08-24 22:32:32 +0530

    08/24/24

--- server/nixos/modules/wireguard.nix
+++ server/nixos/modules/wireguard.nix
@@ -1,6 +1,9 @@
 { pkgs, ...} : 
 {
-
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "enp1s0";
+  networking.nat.internalInterfaces = [ "wg0" ];
+  
   networking.wireguard.enable = true;	
   networking.wireguard.interfaces = {
     # "wg0" is the network interface name. You can name the interface arbitrarily.
@@ -13,14 +16,14 @@
 
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-      #postSetup = ''
-      #  ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
-      #'';
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
+      '';
 
       # This undoes the above command
-      #postShutdown = ''
-      #  ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
-      #'';
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
+      '';
 
       # Path to the private key file.
       #
