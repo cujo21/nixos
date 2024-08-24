f95a7b32c6b31a369093c0da7aebc98705434acb
cujo <akash2110742@ssn.edu.in>
2024-08-24 22:47:02 +0530

    08/24/24

--- server/nixos/modules/wireguard.nix
+++ server/nixos/modules/wireguard.nix
@@ -1,47 +1,56 @@
 { pkgs, ...} : 
 {
-  networking.nat.enable = true;
-  networking.nat.externalInterface = "enp1s0";
-  networking.nat.internalInterfaces = [ "wg0" ];
+  	networking.nate = {
+		enable = true;
+  		externalInterface = "enp1s0";
+  		internalInterfaces = [ "wg0" "wg53" ];
+	};	
   
-  networking.wireguard.enable = true;	
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = [ "10.100.0.1/24" ];
-
-      # The port that WireGuard listens to. Must be accessible by the client.
-      listenPort = 51820;
-
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
-      '';
-
-      # This undoes the above command
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
-      '';
-
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
-      privateKeyFile = "/home/cujo/wireguard-keys/private";
-
-      peers = [
-        # List of allowed peers.
-        { # Feel free to give a meaning full name
-          # Public key of the peer (not a file path).
-          publicKey = "y5x3g/hDwqzqhC64nENtXO4tdweqjGrIglK1J1LscmI=";
-          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-          allowedIPs = [ "10.100.0.2/32" ];
-        }
-      ];
-    };
-  };
+  	networking.wireguard = { 
+			enable = true;	
+  			interfaces = {
+    				wg0 = {
+      
+      					ips = [ "10.100.0.1/24" ];
+      					listenPort = 51820;
+			
+      					postSetup = ''
+      						${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE 
+      					'';
+      					postShutdown = ''
+      						${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
+      					'';
+      					privateKeyFile = "/home/cujo/wireguard-keys/private";
+		
+      					peers = [
+						{
+	  						# PC   
+          						publicKey = "y5x3g/hDwqzqhC64nENtXO4tdweqjGrIglK1J1LscmI=";
+          						allowedIPs = [ "10.100.0.2/32" ];
+       						}
+      					];
+    				};
+				
+				wg53 = {
+					ips = [ "10.100.1.1/24" ];
+					listenPort = 53;
+					
+					postSetup = ''
+        					${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.1.0/24 -o enp1s0 -j MASQUERADE
+      					'';	
+      					postShutdown = ''
+        					${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.1.0/24 -o enp1s0 -j MASQUERADE
+      					'';
+					privateKeyFile = "/home/cujo/wireguard-keys/private";
+					peers = [
+						{
+							# PC 
+							publicKey = "y5x3g/hDwqzqhC64nENtXO4tdweqjGrIglK1J1LscmI=";
+							allowedIPs = [ "10.100.1.2/32" ];
+						}
+					];
+				};	
+  			};
+	};
 
 }
