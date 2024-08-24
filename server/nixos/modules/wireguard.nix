06bfa2fe68dbc9da6d3cbd7a11ef2c451f695b6e
cujo <akash2110742@ssn.edu.in>
2024-08-24 23:09:31 +0530

    08/24/24

--- server/nixos/modules/wireguard.nix
+++ server/nixos/modules/wireguard.nix
@@ -27,6 +27,9 @@
 	  						# PC   
           						publicKey = "y5x3g/hDwqzqhC64nENtXO4tdweqjGrIglK1J1LscmI=";
           						allowedIPs = [ "10.100.0.2/32" ];
+							# Mobile
+							publicKey = "2SCkbWqH/LBCq9+JhxWBT1mY9UhG5MI0ZL5mRotxjRQ=";
+							allowedIPs = [ "10.100.0.3/32" ];
        						}
       					];
     				};
@@ -47,6 +50,9 @@
 							# PC 
 							publicKey = "32J1k4zmR9stwvJZV+Gl86W+3sDcPzsNASnC1fvEkTM=";
 							allowedIPs = [ "10.100.1.2/32" ];
+							# Mobile 
+							publicKey = "9lNEApsiDpmp6WfUDyWSRsTLX9RMxnHHpboIfk9wMnM=";
+							allowedIPs = [ "10.100.1.3/32" ];
 						}
 					];
 				};	
