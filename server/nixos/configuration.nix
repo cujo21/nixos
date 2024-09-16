35883efe484545531c12db57641b988b9d8d0538
cujo <akash2110742@ssn.edu.in>
2024-09-16 18:18:27 +0530

    09/16/24

--- server/nixos/configuration.nix
+++ server/nixos/configuration.nix
@@ -63,41 +63,41 @@ in
 		};
 	};
 	
-#	services.caddy = {
-#		enable = true;
-#		virtualHosts."95661601.xyz".extraConfig = ''
-#			redir /radarr /radarr/
-#		   	redir /jellyfin /jellyfin/
-#			redir /sonarr /sonarr/
-#			redir /prowlarr /prowlarr/
-#			
-#			reverse_proxy /radarr/* {
-#				to 192.168.0.104:7878
-#			}
-#			
-#			reverse_proxy /jellyfin/* {
-#				to 192.168.0.104:8096
-#			}
-#			
-#			reverse_proxy /sonarr/* {
-#				to 192.168.0.104:8989
-#			}
-#
-#			reverse_proxy /prowlarr/* {
-#				to 192.168.0.104:9696
-#			}
-#			
-#			reverse_proxy {
-#				to 192.168.0.104:8080
-#			}
-#		'';
-#		virtualHosts."docker.95661601.xyz".extraConfig = ''
-#			reverse_proxy {
-#				to 192.168.0.104:9000
-#			} 
-#		'';
-#		
-#    	};
+	services.caddy = {
+		enable = true;
+		virtualHosts."95661601.xyz".extraConfig = ''
+			redir /radarr /radarr/
+		   	redir /jellyfin /jellyfin/
+			redir /sonarr /sonarr/
+			redir /prowlarr /prowlarr/
+			
+			reverse_proxy /radarr/* {
+				to 192.168.0.104:7878
+			}
+			
+			reverse_proxy /jellyfin/* {
+				to 192.168.0.104:8096
+			}
+			
+			reverse_proxy /sonarr/* {
+				to 192.168.0.104:8989
+			}
+
+			reverse_proxy /prowlarr/* {
+				to 192.168.0.104:9696
+			}
+			
+			reverse_proxy {
+				to 192.168.0.104:8080
+			}
+		'';
+		virtualHosts."docker.95661601.xyz".extraConfig = ''
+			reverse_proxy {
+				to 192.168.0.104:9000
+			} 
+		'';
+		
+    	};
   services.vsftpd = {
     enable = true;
     writeEnable = true;
