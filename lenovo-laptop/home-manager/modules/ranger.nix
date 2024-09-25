78f0dd6f3bc2d8c95f417e80e4e8f62c8866c884
cujo <akash2110742@ssn.edu.in>
2024-09-25 16:03:10 +0530

    09/25/24

--- lenovo-laptop/home-manager/modules/ranger.nix
+++ lenovo-laptop/home-manager/modules/ranger.nix
@@ -2,6 +2,12 @@
 
   programs.ranger = {
     enable = true;
+    mappings = {
+      yp = "oscyank path";
+      yd = "oscyank dir";
+      yn = "oscyank name";
+      "y." = "oscyank name_without_extension";
+    };
     extraConfig = "
       default_linemode devicons2
     ";
@@ -21,6 +27,13 @@
 	src = builtins.fetchGit {
 	  url = "https://github.com/maximtrp/ranger-archives.git";
 	  rev = "b4e136b24fdca7670e0c6105fb496e5df356ef25";
+	};
+      }
+      {
+	name = "oscyank";
+	src = builtins.fetchGit {
+	  url = "https://github.com/laggardkernel/ranger-oscyank.git";
+	  rev = "4bd84de5fde0b7edb223d62a5a4ee9ef4f1fe472";
 	};
       }
     ];
