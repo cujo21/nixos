908c64064420d70a3b907fe2a460bc1e2338d010
cujo <akash2110742@ssn.edu.in>
2024-09-25 10:37:35 +0530

    09/25/24

--- lenovo-laptop/home-manager/modules/ranger.nix
+++ lenovo-laptop/home-manager/modules/ranger.nix
@@ -2,12 +2,15 @@
 
   programs.ranger = {
     enable = true;
+    extraConfig = "
+      default_linemode devicons2
+    ";
     plugins = [
       {
-	name = "ranger-devicons2"
+	name = "devicons2";
 	src = builtins.fetchGit {
 	  url = "https://github.com/cdump/ranger-devicons2.git";
-	  rev "9606009aa01743768b0f27de0a841f7d8fe196c5";
+	  rev = "9606009aa01743768b0f27de0a841f7d8fe196c5";
 	};
       }
     ];
