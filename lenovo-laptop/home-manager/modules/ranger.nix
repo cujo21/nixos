2f5f71d19d9f8d97c6317e6eee59d0dc1114ae65
cujo <akash2110742@ssn.edu.in>
2024-09-25 15:32:58 +0530

    09/25/24

--- lenovo-laptop/home-manager/modules/ranger.nix
+++ lenovo-laptop/home-manager/modules/ranger.nix
@@ -16,6 +16,13 @@
 	  rev = "9606009aa01743768b0f27de0a841f7d8fe196c5";
 	};
       }
+      {
+	name = "archives";
+	src = builtins.fetchGit {
+	  url = "https://github.com/maximtrp/ranger-archives.git";
+	  rev = "b4e136b24fdca7670e0c6105fb496e5df356ef25";
+	};
+      }
     ];
   };
 
