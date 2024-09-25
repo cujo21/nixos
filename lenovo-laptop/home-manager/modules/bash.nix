c6069151a42cd154f35864b8c79ac98a107cae71
cujo <akash2110742@ssn.edu.in>
2024-09-25 10:38:46 +0530

    09/25/24

--- lenovo-laptop/home-manager/modules/bash.nix
+++ lenovo-laptop/home-manager/modules/bash.nix
@@ -10,6 +10,7 @@
                         hms = "home-manager switch --flake '${flakePath}#cujo'";
 			rebuild = "sudo nixos-rebuild switch --flake '${flakePath}#nixos'";
 			v = "nvim";
+			r = "ranger";
 			lookgc = "looking-glass-client -m 97";
 			wifi_connect = "sudo wpa_supplicant -c /home/cujo/wpa_supplicant.conf -i wlp4s0";
 		};
