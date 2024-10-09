a3d8bd184839cb52375c9c7aa5d273da098a0c75
cujo <akash2110742@ssn.edu.in>
2024-10-09 19:22:46 +0530

    10/09/24

--- lenovo-laptop/home-manager/modules/kitty.nix
+++ lenovo-laptop/home-manager/modules/kitty.nix
@@ -6,23 +6,17 @@
 			enableZshIntegration = true;
 		};
 
-		extraConfig = "
-			#include /home/cujo/nix/lenovo-laptop/home-manager/modules/kitty-themes/catppuccin_mocha.conf
-			shell zsh
-
-			# Window layout
-
-			remember_window_size  no
-			initial_window_width  1280
-			initial_window_height 800
-			hide_window_decorations no
-
-			# Tab bar
-
-			tab_bar_style powerline
-			tab_bar_align left 
-			tab_bar_edge top
-		"; 
+		settings = {
+			#background_opacity = "0.90";
+			shell = "zsh";
+			remember_window_size = false;
+			hide_window_decorations = false;
+			initial_window_width = 1280;
+			initial_window_height = 800;
+			tab_bar_style = "powerline";
+			tab_bar_align = "left";
+			tab_bar_edge = "top";
+		};
 	};
 
 }
