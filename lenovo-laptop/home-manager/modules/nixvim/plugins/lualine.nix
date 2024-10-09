6dc95739ea3dd1da6ecc13ecb7dcfb2292559424
cujo <akash2110742@ssn.edu.in>
2024-10-09 10:15:03 +0530

    10/09/24

--- lenovo-laptop/home-manager/modules/nixvim/plugins/lualine.nix
+++ lenovo-laptop/home-manager/modules/nixvim/plugins/lualine.nix
@@ -1,6 +1,6 @@
 {
-
-	plugins.lualine = {
+	programs.nixvim = {
+		plugins.lualine = {
 			enable = true;
 			
 			settings = {
@@ -18,7 +18,32 @@
 					};
 					always_divide_middle = true;
 				};
+				sections = {
+					lualine_a = [
+						{ __unkeyed-1 = "mode"; }
+					];
+					lualine_b = [
+						{ __unkeyed-1 = "branch"; }
+						{ __unkeyed-1 = "diff"; }
+						{ __unkeyed-1 = "diagnostics"; }
+					];
+					lualine_c = [
+						{ __unkeyed-1 = "filename"; }
+					];
+					lualine_x = [
+						{ __unkeyed-1 = "encoding"; }
+						{ __unkeyed-1 = "fileformat"; }
+						{ __unkeyed-1 = "filetype"; }
+					];
+					lualine_y = [
+						{ __unkeyed-1 = "progrss"; }
+					];
+					lualine_z = [
+						{ __unkeyed-1 = "location"; }
+					];
+				};
 			};
 		};
+	};
 
 }
