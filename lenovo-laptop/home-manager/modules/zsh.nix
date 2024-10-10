e2f1f4183db893673d40c6b1aa26fa6cd1a8018a
cujo <akash2110742@ssn.edu.in>
2024-10-10 10:11:50 +0530

    10/10/24

--- lenovo-laptop/home-manager/modules/zsh.nix
+++ lenovo-laptop/home-manager/modules/zsh.nix
@@ -8,25 +8,25 @@
 
     shellAliases =
       let
-	flakePath = "/home/cujo/nix";
+				flakePath = "/home/cujo/nix";
       in {
-	hms = ''home-manager switch --flake '${flakePath}#cujo' -b "$(date)"'';
-	rebuild = "sudo nixos-rebuild switch --flake '${flakePath}#nixos'";
-	v = "nvim";
-	r = "lf";
-	lookgc = "looking-glass-client -m 97";
-	wifi_connect = "sudo wpa_supplicant -c /home/cujo/wpa_supplicant.conf -i wlp4s0";
-	ls = "lsd";
-	rm = "rm -f";
-    };
+				hms = ''home-manager switch --flake '${flakePath}#cujo' -b "$(date)"'';
+				rebuild = "sudo nixos-rebuild switch --flake '${flakePath}#nixos'";
+				v = "nvim";
+				r = "lf";
+				lookgc = "looking-glass-client -m 97";
+				wifi_connect = "sudo wpa_supplicant -c /home/cujo/wpa_supplicant.conf -i wlp4s0";
+				ls = "lsd";
+				rm = "rm -f";
+		};
 
     prezto = {
       enable = true;
       prompt = {
-	theme = "kylewest";
-      };
+				theme = "kylewest";
+			};
       terminal = {
-	autoTitle = true;
+				autoTitle = true;
       };
     };
 
