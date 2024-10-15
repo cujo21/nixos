143ee0a1bfcccfbdd004edcc6fea3d7b58174d1d
cujo <akash2110742@ssn.edu.in>
2024-10-15 21:48:52 +0530

    10/15/24

--- lenovo-laptop/home-manager/modules/rofi/rofi.nix
+++ lenovo-laptop/home-manager/modules/rofi/rofi.nix
@@ -1,11 +1,23 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
 
 	programs.rofi = {
 		enable = true;
 		package = pkgs.rofi-wayland-unwrapped;
-		font = "Roboto Nerd Font";	
-		theme = "./catppuccin-mocha.rasi";
-		configPath = "./config.rasi";
+		theme = lib.mkForce "./nix/lenovo-laptop/home-manager/modules/rofi/catppuccin-mocha.rasi";
+		extraConfig = {
+				modi = "run,drun,window";
+				show-icons = true;
+				terminal = "kitty";
+				drun-display-format = "{icon} {name}";
+				location = 0;
+				disable-history = false;
+				hide-scrollbar = true;
+				display-drun = "   Apps ";
+				display-run = "   Run ";
+				display-window = " 󰕰  Window";
+				display-Network = " 󰤨  Network";
+				sidebar-mode = true;
+			};
 	};
 
-};
+}
